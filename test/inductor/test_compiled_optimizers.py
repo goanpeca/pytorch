57df34c91ee1dc3167651424e21d4b430563555a
Bob Ren <bobrenjc93@users.noreply.github.com>
2026-04-29 18:19:04 +0000

    Fix Adagrad recompile test perturbation

--- test/inductor/test_compiled_optimizers.py
+++ test/inductor/test_compiled_optimizers.py
@@ -573,10 +573,16 @@ def make_recompile_test(optim_cls, closure=None, kernel_count=2, **kwargs):
                 compiled_step()
 
             # perturb state to force recompile
-            # Adagrad doesn't reinitialize state on each step
             # SGD has an empty state
-            if optim_cls in (Adagrad, SGD):
+            if optim_cls is SGD:
                 opt_compiled.param_groups[0]["lr"] = 0.02
+            elif optim_cls is Adagrad:
+                state_tensor = opt_compiled.state[
+                    opt_compiled.param_groups[0]["params"][0]
+                ]["sum"]
+                opt_compiled.state[opt_compiled.param_groups[0]["params"][0]][
+                    "sum"
+                ] = torch.zeros_like(state_tensor)
             elif optim_cls is Adam:  # ensure we are guarding on the data_ptr of states
                 state_tensor = opt_compiled.state[
                     opt_compiled.param_groups[0]["params"][0]
